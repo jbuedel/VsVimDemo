2e5a5a96bfd832f3d7e3cd80c30e39deaa9b5a67
Josh Buedel <jbuedel@gmail.com>
2012-10-24 17:46:24 -0500

    Added my demo notes and code.

--- VsVimDemo/Controllers/HomeController.cs
+++ VsVimDemo/Controllers/HomeController.cs
@@ -8,25 +8,67 @@ namespace VsVimDemo.Controllers
 {
     public class HomeController : Controller
     {
+        // text mode and insert mode
+        //  * insert for typing, 
+        //  * normal for commands
+        //  * a or i to enter insert mode
+        //  * ESC to get back
+        //  * It WILL DRIVE YOU NUTS.
+
+       // First, here is how to turn it off. :)  Ctrl+Shift+F12
+        
+        // Second, print out this cheet sheet: http://bit.ly/77Xh  (just google vim cheat sheet)
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            // Common scenarios that text editors do not handle well
+
+            // you're inside *something*, and you want to change it.
+
+            // inside a name
+            var foo = DateTime.Now.ToShortDateString(); // ciw
+
+            // inside a string
+            if (true)
+            {
+                ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application."; // ci"
+
+                // inside an indexer ci[
+                ViewBag["stuffkey"] = Stuff(1,3,"stuff"); // or param list ci(
+
+            } // or the whole block  ci{
 
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            // Moving around
+
+            // You're done, but the cursor is in the middle of a line   o (and Shift o)
+            ViewBag.Message = "Your app <cursor>description page.";
+
+            // Navigate 'words' effectively  w e b
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            // Repeating your self - automate repetition
+            // Auto records all activity up until exiting insert mode
+            ViewBag.BlargMessage = "Your contact page.";
+            ViewBag.BlargType= "Your contact page.";
+            ViewBag.BlargZip = "Your contact page.";
+            ViewBag.BlargPhone = "Your contact page.";
 
             return View();
+        }
+
+        private object Stuff(int i, int i1, string stuff)
+        {
+            return new object();    
         }
     }
 }
